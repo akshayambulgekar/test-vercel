ec36b6ee226906b4df8c608b45d6dc8084d0261c
akshay-ambulgekar <akshay.ambulgekar@payppy.co>
2024-09-18 11:55:37 +0530

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# This is a Hello World Code!!
+@app.route('/')
+def hello_world():
+    return 'Hello, Payppy! :)'
+
+
+# initiating the program
+
